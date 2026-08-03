852cdd75219ab97aa52135c2c31aa07978cb28d0
youbing <djanfee@gmail.com>
2026-08-03 11:33:38 +0800

    -feat 增加SortJsonBytes函数

--- utils/encryptx/encryptx.go
+++ utils/encryptx/encryptx.go
@@ -1,8 +1,11 @@
 package encryptx
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
+	"sort"
 	"strings"
 )
 
@@ -17,4 +20,40 @@ func Encrypt(source string) string {
 	data := source + "bcrypt.GenerateFromPassword([]byte(source), bcrypt.DefaultCost)"
 	temp := md5.Sum([]byte(data))
 	return hex.EncodeToString(temp[:])
+}
+
+// SortJsonBytes 解析 JSON 对象，按字段名字符串排序后拼接为紧凑 JSON 字符串。
+func SortJsonBytes(body []byte) (string, error) {
+	body = bytes.TrimSpace(body)
+	if len(body) == 0 {
+		return "", nil
+	}
+
+	var data map[string]json.RawMessage
+	if err := json.Unmarshal(body, &data); err != nil {
+		return "", err
+	}
+
+	keys := make([]string, 0, len(data))
+	for k := range data {
+		keys = append(keys, strings.TrimSpace(k))
+	}
+	sort.Strings(keys)
+
+	var sb strings.Builder
+	sb.WriteByte('{')
+	for i, k := range keys {
+		if i > 0 {
+			sb.WriteByte(',')
+		}
+		keyBytes, err := json.Marshal(k)
+		if err != nil {
+			return "", err
+		}
+		sb.Write(keyBytes)
+		sb.WriteByte(':')
+		sb.Write(bytes.TrimSpace(data[k]))
+	}
+	sb.WriteByte('}')
+	return sb.String(), nil
 }
